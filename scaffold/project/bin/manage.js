53086638e24f7b8c07de846db97dde8cd7ec0cc1
Nathan LaFreniere <quitlahok@gmail.com>
2018-02-06 15:08:16 -0800

    get rid of all the rest of the knork references

--- scaffold/project/bin/manage.js
+++ scaffold/project/bin/manage.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 const path = require('path')
-process.env.KNORK_SETTINGS = path.join(__dirname, '..', 'lib', 'settings')
+process.env.SPIFE_SETTINGS = path.join(__dirname, '..', 'lib', 'settings')
 require('@npm/spife/bin/manage.js')
