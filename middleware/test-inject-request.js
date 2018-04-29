743f84d45626cf5208dfeb7e9a5721ddb64ba83d
Chris Dickinson <christopher.s.dickinson@gmail.com>
2018-04-28 22:52:01 -0700

    it works!

--- middleware/test-inject-request.js
+++ middleware/test-inject-request.js
@@ -8,7 +8,7 @@ function createRequestInjector () {
   return {
     async processSuite (suite, next) {
       suite.request = opts => {
-        return inject(suite.spife.server, opts)
+        return inject(suite.spife.onrequest, opts)
       }
       return next(suite)
     }
